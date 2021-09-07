c48d6e41ac70d05cd38910a9621e9effa3f96411
Jaldhar H. Vyas <jaldhar@braincells.com>
2021-09-07 01:47:32 -0400

    For consistencies sake, move some classes to utils package.

--- app/src/main/java/com/udacity/asteroidradar/api/NeoWSAPI.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NeoWSAPI.kt
@@ -3,7 +3,7 @@ package com.udacity.asteroidradar.api
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.udacity.asteroidradar.Constants.BASE_URL
+import com.udacity.asteroidradar.utils.Constants.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.utils
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
